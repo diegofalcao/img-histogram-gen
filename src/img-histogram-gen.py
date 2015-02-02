7e7c7d2a7ee13805decb47459737d8ecffe2df59
Diego Falcao <diego.falcao@indt.org.br>
2015-02-02 18:21:47 -0400

    Adding method to get the pixel region
    Now, a method was created to get the region of a pixel
    considering that the image is split in 5 regions.

--- src/img-histogram-gen.py
+++ src/img-histogram-gen.py
@@ -1,27 +1,45 @@
 import sys
+import math
+import collections
+
 from PIL import Image
 from PIL import ImageDraw
 
+# It returns the region of an specific point considering that the image was
+# divided into 5 regions.
+def getRegion(width, height, x, y):
+    imgWBorder = width * 0.10
+    imgHBorder = height * 0.10
 
-# Generates K Folds with FAILURE and PASS instances.
-#def generateKFolds(numberOfFolds, numberOfPassInstancesPerFold, passInstances,
-#                   numberOfFailureInstancesPerFold, failureInstances):
+    if (x <= (width / 2) and y <= imgHBorder) or (x <= imgWBorder and y <= height / 2):
+        return "A"
 
-# Combines folds to generate the training and testing sets
-#def generateTrainingAndTestingSetsCombiningFolds(folds, header, trainingPrefixName, testingPrefixName, verbose):
+    if (x > (width / 2) and y <= imgHBorder) or (x > (width - imgWBorder) and y <= height / 2):
+        return "B"
 
-# This script generates folds using StratifiedKFold. It means that
-# it preserves the class distribution of the original file in the generated
-# folds.
+    if (x <= (width / 2) and y > (height - imgHBorder)) or (x <= imgWBorder and y > height / 2):
+        return "D"
+
+    if (x > (width / 2) and y > (height - imgHBorder)) or (x > (width - imgWBorder) and y > height / 2):
+        return "E"
+
+    # It belongs to the square in the center of the image
+    return "C"
+
+# This program breaks the image into 1296 blocks and 5 regions and generates histograms to each block. After that, each
+# histogram block becomes words that can be used as terms in a vector model. So that, it can be used together with a
+# vector model to do CBIR (Content Based Information Retrieval).
 def main(argv):
 
-    # if len(sys.argv) < 5 or len(sys.argv) > 5:
-    #     print "This script generates folds using StratifiedKFold. It means that it preserves the class distribution"
-    #     print "of the original file in the generated folds."
-    #     print "\nCross-validation Stratifield KFold Generator usage:"
-    #     print sys.argv[0], '<csv_file_name> <KFold> <training_output_prefix_name> <testing_output_prefix_name>'
+    # if len(sys.argv) < 2 or len(sys.argv) > 2:
+    #     print "This program breaks the image into 1296 blocks and 5 regions and generates histograms to each block. "
+    #     print "After that, each histogram block becomes words that can be used as terms in a vector model. So that, "
+    #     print "it can be used together with a vector model to do CBIR (Content Based Information Retrieval)."
+    #     print "\nImage Histogram Generator usage:"
+    #     print sys.argv[0], '<image_path>'
     #     sys.exit()
 
+    #im = sys.argv[1]
     im = Image.open('../lena.jpg')
 
     im256 = im.convert('P', palette=Image.ADAPTIVE, colors=255)
@@ -31,31 +49,40 @@ def main(argv):
 
     draw = ImageDraw.Draw(im256)
 
+    print getRegion(width, height, 52, 196)
+
     imgWBorder = width * 0.11
     imgHBorder = height * 0.11
 
-    draw.line((width/2, 0, width/2, imgWBorder), fill=255) # UP
+    draw.line((width/2, 0, width/2, imgHBorder), fill=255) # UP
     draw.line((0, height/2, imgWBorder, height/2), fill=255) # LEFT
     draw.line((width - imgWBorder, height/2, width, height/2), fill=255) # RIGHT
     draw.line((width/2, height - imgHBorder, width/2, height), fill=255) # BOTTOM
 
-    space = 14.2
+    NUMBER_OF_BLOCKS = 1296
+
+    blockSize = math.sqrt((width * height) / NUMBER_OF_BLOCKS)
 
     yleft = 0
-    yright = space
+    yright = blockSize
 
     for i in range(0, 37):
         xleft = 0
-        xright = space
+        xright = blockSize
 
         for j in range(0, 37):
             draw.rectangle((xleft, yleft, xright, yright), outline=0)
 
-            xleft = xleft + space
-            xright = xright + space
+            # tmpImg = im256.crop((xleft, yleft, xright, yright))
+            # tmpHistogram = tmpImg.histogram()
+            # tmpSortedHistogram = collections.OrderedDict(sorted(tmpHistogram.items(), reverse=True))
+            # len95percent = len(sortedHistogram) * 0.95
 
-        yleft = yleft + space
-        yright = yright + space
+            xleft = xleft + blockSize
+            xright = xright + blockSize
+
+        yleft = yleft + blockSize
+        yright = yright + blockSize
 
     draw.rectangle((imgWBorder, imgHBorder, width - imgWBorder, height - imgHBorder), outline=255)
 
